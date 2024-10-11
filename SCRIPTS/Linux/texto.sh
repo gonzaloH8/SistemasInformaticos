eb9dece3ffb9058c25e35f3efeac4e22c2d953e9
Gonzalo <129194237+gonzalo11449@users.noreply.github.com>
2024-10-11 22:52:55 +0200

    Update texto.sh

--- SCRIPTS/Linux/texto.sh
+++ SCRIPTS/Linux/texto.sh
@@ -15,3 +15,14 @@
     echo "Introduce tu edad: "
     read  age -- creamos una variable que obtendra como valor aquello que escriba el usuario por terminal
     echo "Esta es tu edad: $age años" -- 
+
+    #!/bin/bash
+    clear
+    echo El script $0 # Nombre del Script
+    echo Lo esta ejecutando $USER # Variable de entorno
+    echo Y se le esta asignando el PID:$$ # PID del proceso
+    echo Usamos el parametro $1
+    
+    mensaje="Hola mundo"
+    
+    echo Para decir....$mensaje
