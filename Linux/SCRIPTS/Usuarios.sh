e571cede91cbd6fc91684138c8b83dedafb90654
Gonzalo <129194237+gonzaloH8@users.noreply.github.com>
2024-10-25 17:15:14 +0200

    Update Usuarios.sh

--- Linux/SCRIPTS/Usuarios.sh
+++ Linux/SCRIPTS/Usuarios.sh
@@ -107,6 +107,22 @@ echo -e "\n\t\t linea en fichero $COLOR_AMARILLO /etc/passwd $COLOR_RESET"
 grep -e "^$nombre:x:" /etc/passwd
 
 
+#!/bin/bash
+read -p "Introduzca el nombre del usuario (login) " usuario
+existe=0
+for aux in `cat /etc/passwd | cut .f1 -d":"`
+do
+  if [ "aux" == "$usuario" ]
+  then
+      existe=1
+  fi
+done
 
+if [ $existe -eq 1 ]
+then
+    echo "El usuario existe en el sistema"
+else
+  echo "El usuario no existe"
+fi
 
 
