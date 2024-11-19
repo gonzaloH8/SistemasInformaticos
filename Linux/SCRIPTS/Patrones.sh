f1e978fa5169228993000261ee16f0cb797def4f
Gonzalo <129194237+gonzaloH8@users.noreply.github.com>
2024-11-19 13:22:58 +0100

    Update Patrones.sh

--- Linux/SCRIPTS/Patrones.sh
+++ Linux/SCRIPTS/Patrones.sh
@@ -1,3 +1,33 @@
+# PRACTICAS
+# -patron definido asi: ^[0-9]{8}-[a-zA-Z]$ --- que empieze por un numero de 8 numeros con un guion y con una letra
+# ¿Cumplen estos campos el patron?
+# aAfasdf12323 NO --- empieza por letra
+# 12345435645654Z NO --- contiene mas numeros de los permitidos
+# 000-ADFc NO --- no llega a la cantidad de numeros exigido
+# 08982312-T SI ----- cumple con la cantidad de numeros exigida, guion y letra unica exigida
+
+# - define un patron para validar numeros de telefono movil (ej: +34 688 88 11 22, el +34 es opcional)
+# ^(\+[0-9]{2})?[0-9]{9}$
+
+# - define un patron para validar emails: gmail, msn, hotmail, yahoo. gonzasaa8@gmail.com
+# ^.+@(gmail|hotmail|msn|hotmail|yahoo)\.[a-z]{2,3}
+
+# PRACTICA CON GREP Y EXPRESIONES REGULARES
+# - crear en tus Documentos un fichero texto llamado DATOS_CLIENTES.txt con este formato:
+#    nombre_cliente : apellidos_cliente : NIF : tel movil : email
+#    (meter varias lineas,ej):
+#    Gema : Gonzalez Prieto : 08981122-A : +34 677 11 22 33 : gema.gonzalez@educa.madrid.org
+#    Ferando : Martin Perez : 111aad2 : 677112233 : fermartin@gmail.com
+#    Alicia : Gonzalez Jimeno : 08782233Q : 66112233 : @
+#    : : 01234567-Q : +34 666 66 66 66 : devil@gmail.com
+
+# con el comando GREP y con la opcion -e donde pones el patron o expresion regular hay que hacer
+#    - mostrar las lines que tienen todos los campos ok
+#    - mostrar las lineas con campo NIF incorrecto
+#    - mostrar las luineas con campo TEL incorrecto
+#    - mostrar las lineas con campo EMAIL incorrecto
+#          ej: grep -e "^.*$" /home/profesor/Documentos/DATOS_CLIENTES.txt
+
 ARCHIVO leeme.txt
 PATRONES CORRECTOS
 Gema:Gonzalez Prieto:08981122-A:+34 677 11 22 33:gema.gonzalez@educa.madrid.org
