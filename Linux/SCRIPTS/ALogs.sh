7585b8f0ecda14274002f8e79881862656617ea4
Gonzalo <129194237+gonzaloH8@users.noreply.github.com>
2024-11-26 13:26:26 +0100

    Update ALogs.sh

--- Linux/SCRIPTS/ALogs.sh
+++ Linux/SCRIPTS/ALogs.sh
@@ -78,7 +78,20 @@
 # tail -f /tmp/miscript.log -- vemos el texto del script
 
 # PRACTICA
-    rsyslog templates -- plantillas visitar la pagina
-    crear plantillas rsyslog almacene el mensaje que manda el script 
-    y añada la direccion IP donde se manda el mensaje y el nombre del equipo
-    Buscar rsyslog templates
+   #!/bin/bash
+# rsyslog templates -- plantillas visitar la pagina
+# crear plantillas rsyslog almacene el mensaje que manda el script 
+# y añada la direccion IP donde se manda el mensaje y el nombre del equipo
+# Buscar rsyslog templates
+
+# COPIAR EN EL FICHERO RSYSLOG ETC/RSYSLOG.D/50-DEFAULT.CONF
+# template(name="customFormat" type="string" string="%TIMESTAMP% %hostname%: %msg%\n")
+
+clear
+
+    while true
+    do
+        IP_LOCAL=$(hostname -I | awk '{print $1}') # GENERAR LA IP
+        logger -t miscript.sh "IP: $IP_LOCAL - Este es un mensaje desde el script"
+        sleep 1m
+    done
