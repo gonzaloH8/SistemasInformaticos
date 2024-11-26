c1275abc00537f6adb073d3876c64cfa4b6da61a
Gonzalo <129194237+gonzaloH8@users.noreply.github.com>
2024-11-26 12:44:23 +0100

    Update ADirectorios.sh

--- Linux/SCRIPTS/ADirectorios.sh
+++ Linux/SCRIPTS/ADirectorios.sh
@@ -1,21 +1,18 @@
-#! /bin/bash
-: <<'END_COMMENT' 
-- 1 situaros en directorio "Musica" del perfil de tu usuario y ver contenido
-- 2 Crearos dentro del mismo estos directorios
-Musica
-    heavy/instrumental
-    hardcore/trash-metal
-            death-metal
-    dance
-    pop
-    trap
-    regatton
-- 3 ver con tree si todo esta creado ok
-- 4 borrar directorios trap y regatton
-- 5 renombrar directorio pop a Happy-pop 
-- 6 meter algun fichero en directorio Happy-Pop y copiar su contenido en /tmp/Happy-Pop'
-END_COMMENT
-
+#! /bin/bash 
+# 1 situaros en directorio "Musica" del perfil de tu usuario y ver contenido
+# 2 Crearos dentro del mismo estos directorios
+# Musica
+#    heavy/instrumental
+#    hardcore/trash-metal
+#            death-metal
+#    dance
+#    pop
+#    trap
+#    regatton
+# - 3 ver con tree si todo esta creado ok
+# - 4 borrar directorios trap y regatton
+# - 5 renombrar directorio pop a Happy-pop 
+# - 6 meter algun fichero en directorio Happy-Pop y copiar su contenido en /tmp/Happy-Pop'
 clear
 
 echo "Dime una opcion"
