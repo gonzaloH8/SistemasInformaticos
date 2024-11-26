07d09ca84f5714f39e64e6f200193f238eb31b10
Gonzalo <129194237+gonzaloH8@users.noreply.github.com>
2024-11-26 12:55:19 +0100

    Update APatrones.sh

--- Linux/SCRIPTS/APatrones.sh
+++ Linux/SCRIPTS/APatrones.sh
@@ -1,72 +1,33 @@
-# PRACTICAS
-# -patron definido asi: ^[0-9]{8}-[a-zA-Z]$ --- que empieze por un numero de 8 numeros con un guion y con una letra
-# ¿Cumplen estos campos el patron?
-# aAfasdf12323 NO --- empieza por letra
-# 12345435645654Z NO --- contiene mas numeros de los permitidos
-# 000-ADFc NO --- no llega a la cantidad de numeros exigido
-# 08982312-T SI ----- cumple con la cantidad de numeros exigida, guion y letra unica exigida
+# ARCHIVO leeme.txt
+# PATRONES CORRECTOS
+# Gema:Gonzalez Prieto:08981122-A:+34 677 11 22 33:gema.gonzalez@educa.madrid.org
+# Cristina:Peralez Caste:13579012-S:+34 857 05 26 43:Cristina.Peralez@educa.madrid.hotmail
+# Fernando:Santome Pier:91756201-Z:+34 905 84 00 95:Fernando.Santome@educa.madrid.msn
+# Alberto:San Andres Anton:98129410-L:+34 859 83 96 33:Alberto.SanAndresAnton@educa.madrid.es
+# Ana:Caste Miralez:65812059-F:+34 129 85 28 41:Ana.Caste@educa.madrid.com
 
-# - define un patron para validar numeros de telefono movil (ej: +34 688 88 11 22, el +34 es opcional)
-# ^(\+[0-9]{2})?[0-9]{9}$
-
-# - define un patron para validar emails: gmail, msn, hotmail, yahoo. gonzasaa8@gmail.com
-# ^.+@(gmail|hotmail|msn|hotmail|yahoo)\.[a-z]{2,3}
-
-# PRACTICA CON GREP Y EXPRESIONES REGULARES
-# - crear en tus Documentos un fichero texto llamado DATOS_CLIENTES.txt con este formato:
-#    nombre_cliente : apellidos_cliente : NIF : tel movil : email
-#    (meter varias lineas,ej):
-#    Gema : Gonzalez Prieto : 08981122-A : +34 677 11 22 33 : gema.gonzalez@educa.madrid.org
-#    Ferando : Martin Perez : 111aad2 : 677112233 : fermartin@gmail.com
-#    Alicia : Gonzalez Jimeno : 08782233Q : 66112233 : @
-#    : : 01234567-Q : +34 666 66 66 66 : devil@gmail.com
-
-# con el comando GREP y con la opcion -e donde pones el patron o expresion regular hay que hacer
-#    - mostrar las lines que tienen todos los campos ok
-#    - mostrar las lineas con campo NIF incorrecto
-#    - mostrar las luineas con campo TEL incorrecto
-#    - mostrar las lineas con campo EMAIL incorrecto
-#          ej: grep -e "^.*$" /home/profesor/Documentos/DATOS_CLIENTES.txt
-
-ARCHIVO leeme.txt
-PATRONES CORRECTOS
-Gema:Gonzalez Prieto:08981122-A:+34 677 11 22 33:gema.gonzalez@educa.madrid.org
-Cristina:Peralez Caste:13579012-S:+34 857 05 26 43:Cristina.Peralez@educa.madrid.hotmail
-Fernando:Santome Pier:91756201-Z:+34 905 84 00 95:Fernando.Santome@educa.madrid.msn
-Alberto:San Andres Anton:98129410-L:+34 859 83 96 33:Alberto.SanAndresAnton@educa.madrid.es
-Ana:Caste Miralez:65812059-F:+34 129 85 28 41:Ana.Caste@educa.madrid.com
-
-PATRONES INCORRECTOS
-irina:wallace azul:842350183+F:+56 129 53 94 82: 9IrinaWallace@mita.mexico.est
-5Irina:wAllace aZul:8423501834-F:+56345129710: _Irina-Wallace=queri.mexico.es
-_Irina:wallace azul:842350183+F:+56 a14 65 12 83: #IrinaWallace;nica.brasil.com
--IrInA:wallace azul:842350183+F:+42 012 95 49 81: $IrinaWallace,kike.chile.ogi
-Mati:wallace azul:842350183+F:+14 875 58 19 73: %IrinaWallace'loca.argen.est
+# PATRONES INCORRECTOS
+# irina:wallace azul:842350183+F:+56 129 53 94 82: 9IrinaWallace@mita.mexico.est
+# 5Irina:wAllace aZul:8423501834-F:+56345129710: _Irina-Wallace=queri.mexico.es
+# _Irina:wallace azul:842350183+F:+56 a14 65 12 83: #IrinaWallace;nica.brasil.com
+# -IrInA:wallace azul:842350183+F:+42 012 95 49 81: $IrinaWallace,kike.chile.ogi
+# Mati:wallace azul:842350183+F:+14 875 58 19 73: %IrinaWallace'loca.argen.est
 
 #!/bin/bash
-: <<'END_COMMENT'
-    En el archivo leeme.txt busca mediante patrones las siguientes palabras
-    Nombres
-    Apellidos
-    DNI
-    Telefonos
-    Email
-    Linea Completa
-END_COMMENT
 clear
 
 fichero=/home/gonzalo/Escritorio/Scripts/Practicas/leeme.txt
 
 echo "MENU"
-echo "1) Nombres"
-echo "2) Apellidos"
+echo "1) NOMBRES"
+echo "2) APELLIDOS"
 echo "3) DNI"
-echo "4) Telefonos"
-echo "5) Email"
-echo "6) Linea Completa"
+echo "4) TELEFONOS"
+echo "5) EMAIL"
+echo "6) LINEA COMPLETA"
 echo "7) SALIR"
 
-read -p "dime opcion: " opcion
+read -p "Dime opcion: " opcion
 cat $fichero
 
 case $opcion in
