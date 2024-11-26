9062de40325ade5cf78df094af8c7581b4c37593
Gonzalo <129194237+gonzaloH8@users.noreply.github.com>
2024-11-26 13:17:53 +0100

    Update ASeguridad.sh

--- Linux/SCRIPTS/ASeguridad.sh
+++ Linux/SCRIPTS/ASeguridad.sh
@@ -1,13 +1,4 @@
 # PRACTICA
-backup del directorio documentos /home/gonzalo/Documents en fichero /tmp/backup_documentos.tat.gz
-tar -c -v -z -f /tmp/fichero_tar_backup.tar.gz /home/gonzalo/Escritorio/
-
-# PRACTICA
-    mkdir /tmp/mibackup
-    cd /tmp/mibackup
-    tar -x -v -z -f /tmp/backup_documentos.tar.gz
-    
-# PRACTICA
 # creamos script para hacer backup total de directorio Documents de todos los usuarios q no sean del sistema; vamos a programarlo para que se ejecute cada 2 min como tarea cron
 #!/bin/bash
 clear
@@ -17,105 +8,25 @@ clear
        while read linea # almacena cada linea en la variable y la imprime
         do
             idUser=$(echo $linea | cut -d ":" -f 3)
-            nombreUser=$(echo $linea | cut -d ":" .f 1)
+            nombreUser=$(echo $linea | cut -d ":" -f 1)
             if [ $idUser -ge 1000 ]
             then
                 tar -c -v -z -f /tmp/backup_${nombreUser}_$(date '+%Y-%m-%d-%H:%M').tar.gz /home/$nombreUser/Documentos
             fi
         done < /etc/passwd 
 
-# PRACTICA
-#!/bin/bash
-# crear en /tmp un directorio llamado pruebas: dentro de este directorio crear 4 ficheros de texto llamados  
-# creamos backup total con fichero snapshot
-# comprobar con ls q esta creado el fichero tar.gz y el fichero de snapshot
-# mirar el contenido del fichero tar.gz comprobar que tiene los 4 ficheros de texto
-# mirar el contenido del fichero snapshot
-# en el directorio pruebas crear un nuevo fichero llamado LEEME.txt y subdirectorio llamando "otros" Dentro de "otros" meter un fichero lamado: fich_otros.txt
-# creais backup incremental del directorio /tmp/pruebas
-# comprobar con ls que esta creado ficher .tar.gz
-# mirar su contenido !!! SOLO HA DEBIDO COPIAR el fichero: LEEME.txt y /tmp/pruebas/otros/fich_otros.txt
-# restaurar contenido del backup total e icnremental en directorio /tmp/copia_pruebas
-# crear directorio /tmp/copia_pruebas
-# restauramos backup total
-# comprobar con ls q se ha restaurado los ficheros:   fich1.txt fich2.txt fich3.txt fich4.txt
-# restauramos backup incremental
-clear
-
-fich1.txt fich2.txt fich3.txt fich4.txt
-tar -c -v -z -f /tmp/backup_total_pruebas.tar.gz -g /tmp/pruebas_snap /tmp/pruebas/
-tar -c -v -z -f /tmp/backup_incremental_pruebas.tar.gz -g /tmp/pruebas_snap /tmp/pruebas/
-cd /tmp/copia_pruebas
-tar -x -v -z -f /tmp/backup_total_pruebas.tar.gz -g /dev/null
-cd /tmp/copia_pruebas
-tar -x -v -z -f /tmp/backup_incremental_pruebas.tar.gz -g /dev/null
-comprobar con ls q se ha restaurado ya fichero LEEME.txt y directorio "otros" con fichero fich_otros.txt
-
-
-#!/bin/bash
-# PRACTICA SCRIPT
-#    CREACION BACKUP
-# 1. crear backup total aislado ===> pedir el directorio a copiar(comprobar que existe, si no existe, error).
-#                                   pedir el directorio destino del backup(comprobar q existe, si no existe, error)
-#                                   hacer backup con TAR llamado: backup_TOTAL_nombre_directorio_fecha_hora.tar.gz
-# 2. crear backup total+Incremental ====> pedir directorio a copiar(comprobar que existe, si no existe, error)
-#                                         pedir el directorio destino de los backup totales e incrementales(comprobar q existe, si no existe, error)
-#                                         hacer 1º backup total con tar llamado: backup_TOTAL_nombre_directorio_fecha_hora.tar.gz
-#                                           el dichero snap meterlo en directorio destino backups: nombre_directorio_snap
-#                            el propio script debe ser capaz de programar una tarea para q se ejecute al dia siguiente y haga el backup incremental de ese directorio usando ese dichero snap
-# 4. ===SALIR===
-# 5. opcion_
-clear
-
-    echo "MENU DE OPCIONES A EJECUTAR"
-    echo "1) Crear un backup total aislado"
-    echo "2) Crear un backup+Incremental"
-    echo "3) SALIR"
-
-read -p "Dime una opcion: " opcion
-
-case $opcion in
-1)
-    if [ -d /tmp ] && [ -d /home/gonzalo/Escritorio/Scripts ]
-    then
-        echo "Existe el directorio origen y destino, Procedemos a hacer el backup"
-        tar -c -v -z -f /tmp/backup_TOTAL_nombre_directorio_$(date '+%Y-%m-%d-%H:%M').tar.gz /home/gonzalo/Escritorio/Scripts
-        tar -t -v -z -f /tmp/backup_TOTAL_nombre_directorio_$(date '+%Y-%m-%d-%H:%M').tar.gz
-        
-    else
-        echo "El directorio no ha sido encontrado"
-    fi
-;;
-2)
-    if [ -d /tmp ] && [ -d /home/gonzalo/Escritorio/Scripts ]
-    then
-        echo "Existe el directorio origen y destino, Procedemos a hacer el backup total e Incremental"
-        tar -c -v -z -f /tmp/backup_total_pruebas.tar.gz -g /tmp/pruebas_snap /tmp/pruebas/
-        tar -c -v -z -f /tmp/fich_backup_incremental_num.tar.gz -g /tmp/fichero_snap /tmp/pruebas
-    else
-        echo "El directorio no ha sido encontrado"
-    fi
-;;
-
-3)
-    echo "SALIMOS DEL SCRIPT"
-    exit 0
-;;
-esac
-
 # MI PRACTICA
 #!/bin/bash
 clear
-echo "BACKUP TOTAL"
+echo " BACKUP TOTAL "
 echo "1) Crea una carpeta y ficheros en Documents"
 echo "2) Empaqueta el contenido creado en el punto 1"
 echo "3) Mira el contenido"
 echo "4) Restaura el contenido"
 echo "BACKUP INCREMENTAL"
 echo "5) Crea un fichero para el backup Incremental"
-echo "6) Copia el contenido si ha sido modificado"
-echo "7) Borra un archivo y comprueba que se ha actualizado"
-echo "CPIO"
+echo "6) Copia el contenido si ha sido modificado, Borra un archivo y comprueba que se ha actualizado"
+echo " CPIO "
 echo "7) Haz un backup con CPIO"
 echo "8) Extramenos el contenido del backup CPIO"
 echo " RSYNC "
@@ -194,20 +105,19 @@ case $opcion in
 ;;
 esac
 
-
-PRACTICA SERVER
-config serv
-Hay que tener instalado el paquete: openssh-server
-                                    sudo apt install openssh-server
-                                    sudo systemctl enable ssh.service
-                                    sudo systemctl daemon-reload
-                                    sudo systemctl status ssh.service ===> debe estar arrancado
+# PRACTICA SERVER
+CONFIG SERV
+1º) Hay que tener instalado el paquete: openssh-server
+        sudo apt install openssh-server
+        sudo systemctl enable ssh.service
+        sudo systemctl daemon-reload
+        sudo systemctl status ssh.service ===> debe estar arrancado
 2º paso)
     Nos creamos directorio de destino de backups:
         mkdir /tmp/backups_cliente
 
-config cliente
-1º paso: generamos claves publica y privada para cifrar trafico
+CONFIG CLIENTE
+1º paso) generamos claves publica y privada para cifrar trafico
     ssh-keygen -f /home/gonzalo/.ssh/id_rsa -- guardas las claves. Genera un fichero con claves publicas y un fichero con claves privadas
     Enter password-key: ""
     Re-Enter password-key: ""
@@ -215,7 +125,7 @@ config cliente
 Vemos si las ha generado: ls -l /home/gonzalo/.ssh ===> crea dos ficheros: id_rsa(clave privada)
                                                                            id_rsa.pub(clave publica)
 
-2º paso necesitamos subir (copiar) la clave publica al servidor para que pueda descifrar el trafico generado con la clave privada del cliente en la conexion ssh con rsync. 
+2º paso) necesitamos subir (copiar) la clave publica al servidor para que pueda descifrar el trafico generado con la clave privada del cliente en la conexion ssh con rsync. 
     En el servidor el cliente tiene una cuenta llamada: gonza
     (necesito saber la ip del servidor) ejecutar comando: ipconfig o bien: ip address show
         scp /home/user/.ssh/id_rsa.pub user_server@ip_server:/home/user/.ssh/authorized_keys    -- permite copiar ficheros desde una maquina local a una del servidor
