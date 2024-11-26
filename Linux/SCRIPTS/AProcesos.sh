e905c1ad99c0af08ddd32578668d246fe264daeb
Gonzalo <129194237+gonzaloH8@users.noreply.github.com>
2024-11-26 13:06:41 +0100

    Update AProcesos.sh

--- Linux/SCRIPTS/AProcesos.sh
+++ Linux/SCRIPTS/AProcesos.sh
@@ -6,6 +6,7 @@
 # Aumentar su prioridad en 2 puntos con renice, volverlo a comprobar con comando ps
 # Matar el script
 
+INFINITO.SH # script con un bucle infinito
 #!/bin/bash
 clear
 cont=0
@@ -25,48 +26,48 @@ sleep 1s
 done
 
 #! /bin/bash
-    clear
-    
-    app="infinito.sh"
-    echo "MENU"
-    echo "1) consultar en otra consola el valor de su bondad, prioridad y estado (opciones de ps: nice,pri,stat)"
-    echo "2) mandar señal con kill de tipo SIGUSR1 al pid del script ¿que hace el script? "
-    echo "3) suspender el script con CTRL + Z y mandarlo a segundo plano para que continue su ejecucion (comprobarlo con jobs)"
-    echo "4) aumentar su prioridad en 2 puntos con renice, volverlo a comprobar con comando ps "
-    echo "5) matar el script "
-    read opcion
-    case $opcion in
-        1)
-           ps -C $app -o nice,pri,stat
-        ;;
+clear
 
-    2) 
-        ps -C $app -o pid,cmd
-        read -p "escribe el pid: " pid
-        kill -s SIGUSR1 $pid
+app="infinito.sh"
+echo "MENU"
+echo "1) Consultar en otra consola el valor de su bondad, prioridad y estado (opciones de ps: nice,pri,stat)"
+echo "2) Mandar señal con kill de tipo SIGUSR1 al pid del script ¿que hace el script? "
+echo "3) Suspender el script con CTRL + Z y mandarlo a segundo plano para que continue su ejecucion (comprobarlo con jobs)"
+echo "4) Aumentar su prioridad en 2 puntos con renice, volverlo a comprobar con comando ps "
+echo "5) Matar el script "
+read -p "Dime una opcion: " opcion
+case $opcion in
+    1)
+       ps -C $app -o nice,pri,stat
     ;;
 
-    3)
+2) 
+    ps -C $app -o pid,cmd
+    read -p "escribe el pid: " pid
+    kill -s SIGUSR1 $pid
+;;
 
-    ;;
+3)
 
-    4)
-        renice -n +2 $app
-        ps -C $app -o pid,pri,cmd,stat
-    ;;
+;;
 
-    5)
-        ps -C $app -o pid,cmd
-        read -p "Excribe la pid: " pid
-        kill -s SIGTERM $pid
-    ;;
+4)
+    renice -n +2 $app
+    ps -C $app -o pid,pri,cmd,stat
+;;
 
-    *)
-        echo "Salimos del Script"
-    ;;
-    esac
+5)
+    ps -C $app -o pid,cmd
+    read -p "Excribe la pid: " pid
+    kill -s SIGTERM $pid
+;;
 
-# =============================================================================
+*)
+    echo "Salimos del Script"
+;;
+esac
+
+# MI PRACTICA
 #!/bin/bash
 clear
 
@@ -78,7 +79,6 @@ echo "4) Mata el proceso"
 echo "5) SALIR"
 
 read -p "Dime la aplicacion a consultar: " app
-
 read -p "Dime una opcion: " opcion
 case $opcion in
 
@@ -108,5 +108,4 @@ case $opcion in
     echo "Salimos del Script"
     exit 0
 ;;
-
 esac
