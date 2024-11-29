a7af04693b30918b6e0fb155db07139c700ddf8d
Gonzalo <129194237+gonzaloH8@users.noreply.github.com>
2024-11-29 19:18:17 +0100

    Update Tarea_programada.sh

--- Linux/Examen/Archivos/Tarea_programada.sh
+++ Linux/Examen/Archivos/Tarea_programada.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Ejercicio 4 Gonzalo San Andres Anton 1º DAW
 clear
-
+(crontab -l 2>/dev/null ; echo '0 15,22 1,15,30 * 5 ./ejercicio3.sh') | crontab
 * 15,22 1,15,30 * 5 Escritorio/Scripts/Examen/backup_grupos.sh
 fecha=$(date '+%Y-%m-%d__%H:%M')
 logger -p daemon.info -t "[backup_grupos.sh]" "mensaje mandado por script: backup_grupos.sh a las $fecha..."
